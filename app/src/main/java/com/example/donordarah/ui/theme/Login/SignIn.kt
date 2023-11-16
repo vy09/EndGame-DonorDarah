e52eb90f7fe853272b88ddf2b9e374662ddd66a8
DheaAmandaFajriani <dheamandaa203@gmail.com>
2023-11-16 22:08:27 +0700

    update signUp

--- app/src/main/java/com/example/donordarah/ui/theme/Login/SignIn.kt
+++ app/src/main/java/com/example/donordarah/ui/theme/Login/SignIn.kt
@@ -1,12 +1,31 @@
 package com.example.donordarah.ui.theme.Login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.example.donordarah.R
 
-class SignIn : AppCompatActivity() {
+class SignIn : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var signin : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        signin = findViewById(R.id.btn_masuk)
+        signin.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        TODO("Not yet implemented")
+        when(v.id){
+            R.id.btn_masuk->{
+                val intent = Intent(this,signin::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/donordarah/ui/theme/Login/SignUp.kt
@@ -0,0 +1,17 @@
+package com.example.donordarah.ui.theme.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.donordarah.R
+
+class SignUp : AppCompatActivity() {
+
+    private  lateinit var signup : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+
+    }
+}
