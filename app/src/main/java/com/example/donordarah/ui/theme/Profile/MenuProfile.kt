85ffbfb5ccdddcbc014333e033cb9b1c12001dae
Luvy Muhammad <luvymuhammad09@gmail.com>
2023-11-19 03:59:41 +0700

    Update Menu Profile & Edit Profile

---
+++ app/src/main/java/com/example/donordarah/ui/theme/Profile/MenuProfile.kt
@@ -0,0 +1,12 @@
+package com.example.donordarah.ui.theme.Profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.donordarah.R
+
+class MenuProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_profile)
+    }
+}
