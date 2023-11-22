322f2a4cfddfb4861f982c9cead87c3e77dd3c6a
Luvy Muhammad <luvymuhammad09@gmail.com>
2023-11-23 02:31:22 +0700

    Menu & location

---
+++ app/src/main/java/com/example/donordarah/ui/theme/Fitur/StokDarahPMI.kt
@@ -0,0 +1,12 @@
+package com.example.donordarah.ui.theme.Fitur
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.donordarah.R
+
+class StokDarahPMI : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stok_darah_pmi)
+    }
+}
---
+++ app/src/main/java/com/example/donordarah/ui/theme/Profile/RiwayatStokDarah.kt
@@ -0,0 +1,12 @@
+package com.example.donordarah.ui.theme.Profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.donordarah.R
+
+class RiwayatStokDarah : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_riwayat_stok_darah)
+    }
+}
