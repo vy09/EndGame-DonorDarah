1dd72065b85e0861c5cd210279c3dfca8def9ac0
DheaAmandaFajriani <dheamandaa203@gmail.com>
2023-12-02 22:35:58 +0700

    update button

--- app/src/main/java/com/example/donordarah/ui/theme/Login/SignIn.kt
+++ app/src/main/java/com/example/donordarah/ui/theme/Login/SignIn.kt
@@ -4,11 +4,14 @@ package com.example.donordarah.ui.theme.Login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import com.example.donordarah.R
 import com.example.donordarah.ui.theme.Home.Home
+import com.google.android.material.textfield.TextInputEditText
 
 
 class SignIn : AppCompatActivity(), View.OnClickListener {
@@ -16,6 +19,9 @@ class SignIn : AppCompatActivity(), View.OnClickListener {
     private lateinit var btn_signIn : Button
     private lateinit var lupa: TextView
     private lateinit var daftar:TextView
+
+    private lateinit var email : TextInputEditText
+    private lateinit var password : TextInputEditText
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +36,30 @@ class SignIn : AppCompatActivity(), View.OnClickListener {
 
         daftar=findViewById(R.id.btn_daftar)
         daftar.setOnClickListener(this)
+
+        email = findViewById(R.id.edit_email)
+        password = findViewById(R.id.psswd)
+
+        email.addTextChangedListener(loginTextWatcher)
+        password.addTextChangedListener(loginTextWatcher)
+
+
     }
+
+    private var loginTextWatcher = object :TextWatcher{
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+           val user_email = email.text.toString().trim()
+           val user_pass = password.text.toString().trim()
+
+           signin.isEnabled = user_email.isNotEmpty() && user_pass.isNotEmpty()
+        }
+
+        override fun afterTextChanged(s: Editable?) {}
+    }
+
+
 
     override fun onClick(v: View) {
         when(v.id){
--- app/src/main/java/com/example/donordarah/ui/theme/Login/SignUp.kt
+++ app/src/main/java/com/example/donordarah/ui/theme/Login/SignUp.kt
@@ -6,17 +6,26 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import com.example.donordarah.R
 import com.example.donordarah.ui.theme.Home.Home
+import com.google.android.material.textfield.TextInputEditText
 
 class SignUp : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var signup: Button
-    private lateinit var popup : Dialog
     private lateinit var back :ImageView
+
+    private lateinit var namapengguna : TextInputEditText
+    private lateinit var notelp : TextInputEditText
+    private lateinit var email : TextInputEditText
+    private lateinit var alamat : TextInputEditText
+    private lateinit var pass : TextInputEditText
+    private lateinit var password : TextInputEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +33,33 @@ class SignUp : AppCompatActivity(), View.OnClickListener {
 
         signup = findViewById(R.id.btn_daftar)
         signup.setOnClickListener(this)
-//        popup = Dialog(this)
 
         back = findViewById(R.id.btnback)
         back.setOnClickListener(this)
+
+        namapengguna.addTextChangedListener(loginTextWatcher)
+        notelp.addTextChangedListener(loginTextWatcher)
+        email.addTextChangedListener(loginTextWatcher)
+        alamat.addTextChangedListener(loginTextWatcher)
+        pass.addTextChangedListener(loginTextWatcher)
+        password.addTextChangedListener(loginTextWatcher)
+    }
+    private var loginTextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            val user_email = email.text.toString().trim()
+            val user_nampeg = namapengguna.text.toString().trim()
+            val user_notelp = notelp.text.toString().trim()
+            val user_alamat = alamat.text.toString().trim()
+            val user_pass = pass.text.toString().trim()
+            val user_passw = password.text.toString().trim()
+
+            signup.isEnabled = user_email.isNotEmpty() && user_pass.isNotEmpty() && user_nampeg.isNotEmpty() &&
+                    user_notelp.isNotEmpty() && user_alamat.isNotEmpty() && user_passw.isNotEmpty()
+        }
+
+        override fun afterTextChanged(s: Editable?) {}
     }
 
     override fun onClick(v: View) {
