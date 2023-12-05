8f283e6fb58f06d329f37221e8d814c9febacf70
Luvy Muhammad <luvymuhammad09@gmail.com>
2023-12-05 16:58:12 +0700

    UPDATE stok darah

--- app/src/main/java/com/example/donordarah/ui/theme/Fitur/StokDarahPMI.kt
+++ app/src/main/java/com/example/donordarah/ui/theme/Fitur/StokDarahPMI.kt
@@ -1,12 +1,29 @@
 package com.example.donordarah.ui.theme.Fitur
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import com.example.donordarah.R
+import com.example.donordarah.ui.theme.Home.Home
 
-class StokDarahPMI : AppCompatActivity() {
+class StokDarahPMI : AppCompatActivity(), View.OnClickListener {
+    private lateinit var btnback236 : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stok_darah_pmi)
+
+        btnback236 = findViewById(R.id.btnback236)
+        btnback236.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btnback236 -> {
+                val intent = Intent(this, MenuStokDarah::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
