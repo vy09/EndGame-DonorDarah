b09b444e73b77ce95eae61dd44445c2b7ce30b8a
DheaAmandaFajriani <dheamandaa203@gmail.com>
2023-12-17 00:55:30 +0700

    login google2

--- app/src/main/java/com/example/donordarah/ui/theme/Login/SignIn.kt
+++ app/src/main/java/com/example/donordarah/ui/theme/Login/SignIn.kt
@@ -11,19 +11,27 @@ import android.widget.Button
 import android.widget.TextView
 import com.example.donordarah.R
 import com.example.donordarah.ui.theme.Home.Home
-import com.google.android.material.textfield.TextInputEditText
-
+import com.example.donordarah.databinding.ActivitySignInBinding
 
 class SignIn : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var btn_signIn : Button
+    private lateinit var btn_signIn: Button
+    private lateinit var binding: ActivitySignInBinding
     private lateinit var lupa: TextView
-    private lateinit var daftar:TextView
-
+    private lateinit var pref: customSharePreference
+    private lateinit var daftar: TextView
+    private lateinit var google: GoogleSignInAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        pref = customSharePreference(this@SignIn)
+        google = GoogleSignInAuth(this, binding.pbSigninGoogle)
+        google.initialize()
+
 
         btn_signIn = findViewById(R.id.btn_signIn)
         btn_signIn.setOnClickListener(this)
@@ -31,26 +39,38 @@ class SignIn : AppCompatActivity(), View.OnClickListener {
         lupa = findViewById(R.id.lupaPW)
         lupa.setOnClickListener(this)
 
-        daftar=findViewById(R.id.btn_daftar)
+        daftar = findViewById(R.id.btn_daftar)
         daftar.setOnClickListener(this)
+
+        binding.LoginGoogle.setOnClickListener(this)
 
     }
 
     override fun onClick(v: View) {
-        when(v.id){
-            R.id.btn_signIn ->{
+        when (v.id) {
+
+            R.id.btn_signIn -> {
+                pref.saveLogin(1).let {
                 val intent = Intent(this, Home::class.java)
                 startActivity(intent)
             }
+            }
 
-            R.id.lupaPW ->{
+            R.id.lupaPW -> {
                 val intent = Intent(this, gantisandi::class.java)
                 startActivity(intent)
             }
 
-            R.id.btn_daftar ->{
-                val intent= Intent(this, SignUp::class.java)
+            R.id.btn_daftar -> {
+                val intent = Intent(this, SignUp::class.java)
                 startActivity(intent)
+            }
+
+            R.id.LoginGoogle -> {
+                pref.saveLogin(1).let {
+                    binding.pbSigninGoogle.visibility = View.VISIBLE
+                    google.signInGoogle()
+                }
             }
         }
     }
